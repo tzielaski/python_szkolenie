f2fb2b20db4d74c071fd1e5929e2775f0b385fea
Tomasz Zielański <ziolo320t@gmail.com>
2018-09-10 12:50:25 +0200

    Simple ifs and operators

---
+++ control_input.py
@@ -0,0 +1,8 @@
+MATURITY_AGE = 18.
+
+user_age = float(input("Input your age: "))
+
+if user_age >= MATURITY_AGE:
+    print("You're an adult already.")
+else:
+    print("You're to become adult yet.")
---
+++ operators_integers.py
@@ -0,0 +1,6 @@
+number = float(input("Number: "))
+
+if number % 1 == 0:
+    print("Whole number")
+else:
+    print("Float")
